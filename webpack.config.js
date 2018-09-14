9e12bc377292e885675e006dda23cbbc8e398e0b
rook91 <rook01093@gmail.com>
2018-09-14 17:19:22 +0200

    init

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const webpack = require('webpack');
+
+module.exports = [{
+    entry: './src/index.jsx',
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: [
+                            "@babel/env",
+                            "@babel/preset-react"
+                        ]
+                    }
+                }
+            }
+        ]
+    },
+    resolve: {
+        extensions: ['*', '.js', '.jsx']
+    },
+    plugins: [
+        new CopyWebpackPlugin([{
+            from: './src/index.html'
+        }]),
+        new webpack.LoaderOptionsPlugin({
+            minimize: true,
+            debug: false
+        }),
+        new webpack.EnvironmentPlugin({
+            NODE_ENV: 'production',
+            DEBUG: false
+        }),
+        new webpack.optimize.ModuleConcatenationPlugin()
+    ],
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+}];
