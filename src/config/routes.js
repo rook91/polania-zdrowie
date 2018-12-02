a504f3d0e26c65b6340967ab4f1fe2f904bf2f34
rook91 <rook01093@gmail.com>
2018-12-02 14:32:14 +0100

    eslint support

--- src/config/routes.js
+++ src/config/routes.js
@@ -1,8 +1,8 @@
 export default [
-    {name: 'home', path: '/', displayText: 'STRONA GŁÓWNA'},
-    {name: 'about', path: '/o_firmie', displayText: 'O FIRMIE'},
-    {name: 'offer', path: '/nasza_oferta', displayText: 'NASZA OFERTA'},
-    {name: 'literature', path: '/literatura', displayText: 'LITERATURA'},
-    {name: 'prizes', path: '/cennik', displayText: 'CENNIK'},
-    {name: 'contact', path: '/kontakt', displayText: 'KONTAKT'},
+  { name: 'home', path: '/', displayText: 'STRONA GŁÓWNA' },
+  { name: 'about', path: '/o_firmie', displayText: 'O FIRMIE' },
+  { name: 'offer', path: '/nasza_oferta', displayText: 'NASZA OFERTA' },
+  { name: 'literature', path: '/literatura', displayText: 'LITERATURA' },
+  { name: 'prizes', path: '/cennik', displayText: 'CENNIK' },
+  { name: 'contact', path: '/kontakt', displayText: 'KONTAKT' },
 ];
